beadcad70822e11c3d22e70fb53e6b606551b303
Pablo Soriano Antón <10006712.joan23@fje.edu>
2021-03-10 18:13:43 +0100

    Diseño, recogida de CP

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -6,9 +6,36 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
-class WeatherController extends Controller
-{
+class WeatherController extends Controller{
     public function login(){
         return view('login');
+    }
+
+    public function buscar(){
+        if($_GET["cpostal"]==null) {
+            return redirect('login');
+            die();
+        }
+        // return view('vista');
+        $datos = file_get_contents ("http://api.openweathermap.org/data/2.5/weather?zip=".$_GET['cpostal'].",es&units=metric&appid=6682dabc389796ef2723e3330b13900a&lang=es");
+        $json = json_decode($datos);
+        $ciudad = $json->name;
+        // $lat = $json->coord->lat;
+        // $lon = $json->coord->lon;
+        $temp = $json->main->temp;
+        $tempmax = $json->main->temp_max;
+        $tempmin = $json->main->temp_min;
+        $presion = $json->main->pressure;
+        $humedad = $json->main->humidity;
+        // $vel_viento = $json->main->temp;
+        $estado_cielo = $json->weather[0]->main;
+        $descripcion = $json->weather[0]->description;
+
+        echo "La temperatura en ".$ciudad."es de ".$temp;
+        echo "Estado del cielo: ".$estado_cielo;
+        echo "Descripción: ". $descripcion;
+        echo "Temperatura: ".$temp." K [Máx: ".$tempmax."K, Mín: ".$tempmin."K]";
+        echo "Presión: ".$presion;
+        echo "Humedad: ".$humedad;
     }
 }
